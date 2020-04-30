3193dd6eb05ec98d484397e4185d2c281c5faae0
Rick Osowski <rosowski@gmail.com>
2020-04-30 14:59:36 -0500

    First pass at 'Concepts'
    Signed-off-by: Rick Osowski <rosowski@gmail.com>

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -2,32 +2,21 @@ module.exports = {
   siteMetadata: {
     title: 'IBM Garage Event-Driven Reference Architecture',
     description: 'This project represents the body of knowledge around event-driven architecture and can be considered as a live book, we are writing from our consulting engagements',
-    keywords: 'gatsby,theme,carbon',
-    repository: {
-      baseUrl: 'https://github.com/ibm-cloud-architecture/refarch-eda',
-      subDirectory: '/docs',
-      branch: 'master'
-    }
+    keywords: 'gatsby,theme,carbon'
   },
   pathPrefix: `/refarch-eda`,
   plugins: [
     {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'Carbon Design Gatsby Theme',
-        short_name: 'Gatsby Theme Carbon',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#0062ff',
-        display: 'browser',
-      },
-    },
-    {
       resolve: 'gatsby-theme-carbon',
       options: {
-       isSearchEnabled: true,
-       titleType: 'append'
-      },
+        isSearchEnabled: true,
+        titleType: 'append',
+        repository: {
+          baseUrl: 'https://github.com/ibm-cloud-architecture/refarch-eda',
+          subDirectory: '/docs',
+          branch: 'master'
+        }
+      }
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
