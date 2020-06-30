0a75f4d162aba44e45c9395989eef4a3b0bf55c0
Rick Osowski <rosowski@gmail.com>
2020-06-30 16:59:26 -0500

    Landing Page refactor
    Signed-off-by: Rick Osowski <rosowski@gmail.com>

---
+++ docs/src/gatsby-theme-carbon/templates/Homepage.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { HomepageBanner, HomepageCallout } from 'gatsby-theme-carbon';
+import HomepageTemplate from 'gatsby-theme-carbon/src/templates/Homepage';
+import { calloutLink } from './Homepage.module.scss';
+
+//import Carbon from '../../images/developer-main.jpg';
+import Carbon from '../../images/event-driven-main.jpg';
+
+const FirstLeftText = () => <p>What are Event-Driven Architectures?</p>;
+
+const FirstRightText = () => (
+  <p>
+    Event-driven architectures provide a collection of loosely-coupled software components, cooperatively reacting to unending streams of events occurring throughout the world.
+    <a
+      className={calloutLink}
+      href="/introduction/overview/"
+    >
+      Overview →
+    </a>
+  </p>
+);
+
+const SecondLeftText = () => <p>Garage</p>;
+
+const SecondRightText = () => (
+  <p>
+    IBM Garage™ is a bold, comprehensive approach to innovation and transformation that brings designers and developers together with your business and IT stakeholders to quickly create and scale new ideas that can dramatically impact your business.
+    <a
+      className={calloutLink}
+      href="https://www.ibm.com/garage"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      IBM Garage →
+    </a>
+  </p>
+);
+
+const BannerText = () => <h1>IBM Garage for Cloud Event-Driven Reference Architecture</h1>;
+
+const customProps = {
+  Banner: <HomepageBanner renderText={BannerText} image={Carbon} />,
+  FirstCallout: (
+    <HomepageCallout
+      backgroundColor="#030303"
+      color="white"
+      leftText={FirstLeftText}
+      rightText={FirstRightText}
+    />
+  ),
+  SecondCallout: (
+    <HomepageCallout
+      leftText={SecondLeftText}
+      rightText={SecondRightText}
+      color="white"
+      backgroundColor="#061f80"
+    />
+  ),
+};
+
+// spreading the original props gives us props.children (mdx content)
+function ShadowedHomepage(props) {
+  return <HomepageTemplate {...props} {...customProps} />;
+}
+
+export default ShadowedHomepage;
