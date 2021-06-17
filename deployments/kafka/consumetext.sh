1d7819d395108b43d1ff5eee009993ab59cb79a5
Jerome Boyer <jerome.boyer@gmail.com>
2021-06-17 15:19:18 -0700

    clean old deployment - reorg fit for purpose

--- deployments/kafka/consumetext.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -z "$POD" ]
-then
-  echo "get kafka pod name"
-  export POD=$(kubectl get pods | grep gc-kafka | awk '{print $1}')
-fi
-echo $POD
-kubectl exec $POD -- bash -c "/opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server  localhost:32224 --topic test-topic --from-beginning"
